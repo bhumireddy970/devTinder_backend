9cef23b528d5a4d3964ff634517e89de4bbf38f1
Saradhi <bhumireddysaradhi9705@gmail.com>
2025-05-11 18:05:33 +0530

    added post,get

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Saradhi", lastname: "Bhumireddy" });
+});
+app.post("/user", (req, res) => {
+  res.send("Data has been posted");
+});
+app.delete("/user", (req, res) => {
+  res.send("Data has been deleted ");
+});
+
 app.use("/test", (req, res) => {
   res.send("test path has called");
-});
-app.use("/", (req, res) => {
-  res.send("Base path has called");
 });
 
 app.listen(3000, () => {
